cd59a5ff18af216c49c0fb47f168ce38c477c6fc
ARIAN POPALYAR <arian_popalyar@yahoo.com>
2018-06-19 17:21:19 +0100

    changes to the burger builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,10 @@
-import Rect from 'react';
+import React from 'react';
 import Aux from '../../hoc/Aux';
-
+import classes from './Layout.css';
 const layout = ( props ) => (
     <Aux>
         <div>Toolbar, sideDrawer, BAckdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
